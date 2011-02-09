1c9dd39edc34d103ed1bbcb8d9b90dc73ef9c34f
koksneo <zadaniematma@interia.pl>
2011-02-09 17:58:41 +0100

    [a482] Support for quest Mystery of the infinite * Support for 'Future you' is currently missing

--- src/bindings/ScriptDev2/scripts/northrend/dragonblight.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/dragonblight.cpp
@@ -419,6 +419,90 @@ CreatureAI* GetAI_mob_maggot(Creature* pCreature)
     return new mob_maggotAI(pCreature);
 }
 
+/*######
+## npc_hourglass_of_eternity
+######*/
+/*Support for 'Future you' is currently missing*/ 
+enum
+{
+    NPC_INFINITE_CHRONO_MAGUS    = 27898,
+    NPC_INFINITE_ASSAILANT       = 27896,
+    NPC_INFINITE_DESTROYER       = 27897,
+    NPC_INFINITE_TIMERENDER      = 27900,
+    QUEST_MYSTERY_OF_INFINITE    = 12470
+
+};
+
+struct MANGOS_DLL_DECL npc_hourglassAI : public ScriptedAI
+{
+    npc_hourglassAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
+    
+    uint64 uiWaveTimer;
+    uint32 uiWaveCounter;
+
+     void Reset()
+     {
+         uiWaveTimer = 5000;
+         uiWaveCounter = 0;
+     }
+
+     void JustSummoned(Creature* pSummoned)
+     {
+         pSummoned->AI()->AttackStart(m_creature);
+     }  
+
+     void JustDied(Unit* pKiller)
+     {
+        if(Player *pPlayer = m_creature->GetMap()->GetPlayer(m_creature->GetOwnerGUID()))
+        {
+            pPlayer->FailQuest(QUEST_MYSTERY_OF_INFINITE);
+        }
+     }
+
+     void SummonWave()
+     {
+         switch(uiWaveCounter)
+         {
+            case 0: m_creature->SummonCreature(NPC_INFINITE_CHRONO_MAGUS, m_creature->GetPositionX()+5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    m_creature->SummonCreature(NPC_INFINITE_ASSAILANT, m_creature->GetPositionX()-5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    break;
+            case 1: m_creature->SummonCreature(NPC_INFINITE_CHRONO_MAGUS, m_creature->GetPositionX()+5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    m_creature->SummonCreature(NPC_INFINITE_CHRONO_MAGUS, m_creature->GetPositionX()-5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    break;
+            case 2: m_creature->SummonCreature(NPC_INFINITE_CHRONO_MAGUS, m_creature->GetPositionX()+5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    m_creature->SummonCreature(NPC_INFINITE_ASSAILANT, m_creature->GetPositionX()-5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    m_creature->SummonCreature(NPC_INFINITE_DESTROYER, m_creature->GetPositionX()+5,m_creature->GetPositionY()+5 ,m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    break;
+            case 3: m_creature->SummonCreature(NPC_INFINITE_CHRONO_MAGUS, m_creature->GetPositionX()+5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    m_creature->SummonCreature(NPC_INFINITE_ASSAILANT, m_creature->GetPositionX()-5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    m_creature->SummonCreature(NPC_INFINITE_DESTROYER, m_creature->GetPositionX()+5,m_creature->GetPositionY()+5 ,m_creature->GetPositionZ(), 0, TEMPSUMMON_CORPSE_DESPAWN, 0);
+                    break;
+            case 4: m_creature->SummonCreature(NPC_INFINITE_TIMERENDER, m_creature->GetPositionX()+5,m_creature->GetPositionY(),m_creature->GetPositionZ(), 0, TEMPSUMMON_TIMED_OR_CORPSE_DESPAWN, 300000);
+                    break;
+
+         }
+     }
+     void UpdateAI(const uint32 uiDiff)
+     {
+            if (uiWaveTimer <= uiDiff)
+            {
+                if(uiWaveCounter<=4)
+                {
+                    SummonWave();
+                    uiWaveTimer = 15000;
+                    uiWaveCounter++;
+                }
+                else m_creature->ForcedDespawn();
+
+            } else uiWaveTimer -= uiDiff;
+     }
+};
+
+CreatureAI* GetAI_npc_hourglass(Creature* pCreature)
+{
+    return new npc_hourglassAI(pCreature);
+}
+
 void AddSC_dragonblight()
 {
     Script *newscript;
@@ -466,5 +550,10 @@ void AddSC_dragonblight()
     newscript = new Script;
     newscript->Name = "mob_maggot";
     newscript->GetAI = &GetAI_mob_maggot;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_hourglass";
+    newscript->GetAI = &GetAI_npc_hourglass;
     newscript->RegisterSelf();
 }
