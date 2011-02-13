608594d9d5a0b35f0d4d2441f4a0037d86283171
inteliboy <inteliboy@github.com>
2011-02-13 13:25:32 +0100

    [a497] Hunter's Certicate of Ownership

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -9175,7 +9175,7 @@ void Spell::EffectRenamePet(SpellEffectIndex /*eff_idx*/)
         !((Creature*)unitTarget)->IsPet() || ((Pet*)unitTarget)->getPetType() != HUNTER_PET)
         return;
 
-    unitTarget->RemoveByteFlag(UNIT_FIELD_BYTES_2, 2, UNIT_CAN_BE_RENAMED);
+    unitTarget->SetByteFlag(UNIT_FIELD_BYTES_2, 2, UNIT_CAN_BE_RENAMED);
 }
 
 void Spell::EffectPlayMusic(SpellEffectIndex eff_idx)
