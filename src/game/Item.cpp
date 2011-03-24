15438c80c2cadc3c807b6265cd0b36f845345461
koksneo <zadaniematma@interia.pl>
2011-03-24 23:07:22 +0100

    [a520] Support for item Mysterious Egg and Disgusting Jar. From TrinityCore.

--- src/game/Item.cpp
+++ src/game/Item.cpp
@@ -280,7 +280,13 @@ void Item::UpdateDuration(Player* owner, uint32 diff)
 
     if (GetUInt32Value(ITEM_FIELD_DURATION)<=diff)
     {
+        // variables to hold item location and entry data after item gets destroyed
+        uint32 itemEntry = GetEntry();
+        uint8 bagSlot = GetBagSlot();
+        uint8 slot = GetSlot();
+        uint16 pos = GetPos();
         owner->DestroyItem(GetBagSlot(), GetSlot(), true);
+        owner->HandleDestroyItemReplace(itemEntry, bagSlot, slot, pos);
         return;
     }
 
